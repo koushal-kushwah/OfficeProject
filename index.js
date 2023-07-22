d9bec5de9c0794c4ac41a5863bb911bc8ab8cbf0
koushal-kushwah <koushalkushwah12@gmail.com>
2023-07-22 08:22:04 +0530

    token init

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express")
+const dotenv = require("dotenv")
 const authRouter = require("./routers/AuthRouter")
 
+
+dotenv.config();
 const server = express("")
 server.use(express.json())
 
 server.get("/",(req,res)=>{
-
+    res.json("Server is running.....")
 })
 
 server.use("/auth",authRouter)
 
-server.listen(8080,()=>{
-    console.log("http://localhost:8080")
+server.listen(process.env.PORT,()=>{
+    console.log("http://localhost:"+process.env.PORT)
 })
--- routers/AuthRouter.js
+++ routers/AuthRouter.js
@@ -1,11 +1,49 @@
 const express = require("express")
+const jwtManager = require("../config/JwtManager")
+const {User} =require("../models/index")
+const ApiResponse = require("./ApiResponse")
+const WebRouter = require("./WebRouter")
+
 const router = express.Router()
 
 
-router.post("/login",(req,res)=>{
-    
+router.post("/login", async(req,res)=>
+{
+    const {email,password} = req.body;
+    const user = await User.findOne({
+        where : {email,password},
+        include : "employee",
+        attributes:{
+        exclude : ["emp_id","createdAt","updatedAt"]
+        }
+    });
+    if(user==null){
+        res.json(new ApiResponse(false,null,"Invalid User !",
+        null))
+    }
+    else{
+        var userid = user.id;
+        var empid = user.employee.id;
+        var type = user.employee.type;
+        var name = user.employee.type;
+
+        const token = jwtManager.generateAccessToken(userid,
+            empid,type);
+
+        res.json(new ApiResponse(true,{name,token},"Welcome User !",
+        null))
+    }
 })
 
+router.use("*",(req,res,next)=>{
+    jwtManager.authenticateToken(req,result=>{
+        if(result.status)
+            next()
+        else
+            res.json(new ApiResponse(result.status,null,result.msg,null))
+    })
+})
 
+router.use("/web",WebRouter)
 
 module.exports =router;
